7e04b02dd7e6fe46de2c67ce69d95b3038eecbdc
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2018-10-15 22:15:43 -0500

    List

---
+++ Interview/List/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List< int> list = new List<int>();
+
+            list.Add(2);
+            list.Add(3);
+            list.Add(7);
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
