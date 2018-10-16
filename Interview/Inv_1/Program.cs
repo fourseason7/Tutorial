a77d473a1f353f202a7eee4802ca72562ae1c344
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2018-10-15 21:57:12 -0500

    Sample Code
    Interview Program Sample

---
+++ Interview/Inv_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Inv_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> lists = new List<string>();
+            //lists.Add("CCGGATGGCGGCGTCACTGTC");
+            //lists.Add("CCGGATGGCGGCGTCGCTGTC");
+            //lists.Add("CGGGTTGGCGGCGTCGCTGTC");
+            //lists.Add("CAGGTTGGCGGCGTCACTGTC");
+            //lists.Add("CTGGTTGGTGGCGTCACTGTC");
+            //lists.Add("CGGGTTGGTGGCGTCGCTGTC");
+            //lists.Add("CGGGGTGGTGGCGTCGCTGTC");
+
+            List<string> lists = new List<string>();
+            var dataFile = File.ReadAllLines(@"d:\Test\seqdata.txt");
+            foreach (var item in dataFile)
+            {
+                lists.Add(item);
+            }
+
+            string strToMatch = "CSGGWTGGYGGCGTCRCTGTC";
+
+            strToMatch = strToMatch.Replace("M", "[AC]");
+            strToMatch = strToMatch.Replace("R", "[AG]");
+            strToMatch = strToMatch.Replace("W", "[AT]");
+            strToMatch = strToMatch.Replace("S", "[CG]");
+            strToMatch = strToMatch.Replace("Y", "[CT]");
+            strToMatch = strToMatch.Replace("K", "[GT]");
+
+            Console.WriteLine("String to Match: " + strToMatch);
+
+
+            foreach (string item in lists)
+            {
+                if (Regex.IsMatch(item, strToMatch))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interview/Inv_2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Inv_2
+{
+    class Program
+    {
+        static Dictionary<string, string> dictionary1 = new Dictionary<string, string>();
+        //static Dictionary<string, string> dictionary2 = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            string filePath = @"d:\Test";
+
+            // file name
+
+
+            //string[] files = Directory.GetFiles(filePath, "*.fastq");
+            string[] files = Directory.GetFiles(filePath);
+            foreach (string item in files)
+            {
+                //Console.WriteLine(item.ToString());
+
+                if (item.Contains("_Read1"))
+                {
+                    getData(item, 1);
+                }
+                else if (item.Contains("_Read2"))
+                {
+                    getData(item, 2);
+                }
+
+            }
+
+            foreach (string key in dictionary1.Keys)
+            {
+                Console.WriteLine("Sequence at the coordinates " + key + " " + dictionary1[key]);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void getData(string fileName, int filenumber)
+        {
+            // line 1 : key 1, 5, 9, 13 - using mod 4
+            // line 2 : value 2, 6, 10, 14 - using mod 4
+            string key = string.Empty;
+            string value = string.Empty;
+
+            string xvalue = string.Empty;
+            string yvalue = string.Empty;
+
+            foreach (var item in File.ReadLines(fileName)
+                                        .Select((text, index) => new {text, linenumber = index + 1}))
+                                  
+            {
+                if ((item.linenumber % 4) == 1) //key
+                {
+                    key = item.text;
+
+                    key = key.Substring(0, key.IndexOf(" "));
+                    yvalue = key.Substring(key.LastIndexOf(":" )+ 1);
+                    key = key.Substring(0, key.LastIndexOf(":"));
+                    xvalue = key.Substring(key.LastIndexOf(":") + 1);
+
+                    key = string.Format("{0}:{1}", xvalue, yvalue);
+
+                }
+                else if ((item.linenumber % 4) == 2) // value
+                {
+                    value = item.text;
+                }
+                else if((item.linenumber % 4) == 0 ) //value finish
+                {
+                    if (filenumber == 1)
+                    {
+                        if (!dictionary1.ContainsKey(key))
+                        {
+                            dictionary1.Add(key, value);
+                        }
+                    }
+                    else if (filenumber == 2)
+                    {
+                        if (!dictionary1.ContainsKey(key))
+                        {
+                            dictionary1.Add(key, value);
+                        }
+                        else
+                        {
+                            dictionary1[key] += value;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
