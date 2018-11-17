deee39e1a3cfe30b2aa2bf7d9e7e1ade20e34aa1
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2018-11-16 21:02:06 -0600

    {add models}

--- AspnetCoreStudy/src/AspnetCoreStudy/Controllers/HomeController.cs
+++ AspnetCoreStudy/src/AspnetCoreStudy/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetCoreStudy.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspnetCoreStudy.Controllers
@@ -10,6 +11,23 @@ namespace AspnetCoreStudy.Controllers
     {
         public IActionResult Index()
         {
+            //var firstUser = new User();
+
+            var hongUser = new User
+            {
+                UserNo = 1,
+                UserName = "Hong, Gildong"
+            };
+
+            // first way
+            //return View(hongUser);
+
+            // second way
+            //ViewBag.User = hongUser;
+
+            // Thired way
+            ViewData["UserNo"] = hongUser.UserNo;
+            ViewData["UserName"] = hongUser.UserName;
             return View();
         }
 
