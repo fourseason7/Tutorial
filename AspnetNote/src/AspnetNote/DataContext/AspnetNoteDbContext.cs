4b2e0962d287c10081a9f7689f6fd9979d0878ad
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2018-11-18 00:04:32 -0600

    AspnetCoreStudy

---
+++ AspnetNote/src/AspnetNote/DataContext/AspnetNoteDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AspnetNote.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetNote.DataContext
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Data Source=localhost;Initial Catalog=NorthWind;Persist Security Info=True;User ID=sa;Password=***********
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=AspnetNoteDb;User Id=sa;Password = kkh19797;");
+        }
+
+    }
+}
---
+++ AspnetNote/src/AspnetNote/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspnetNote.Models
+{
+    public class Note
+    {
+        /// <summary>
+        /// Note No
+        /// </summary>
+        [Key]
+        public int NoteNo { get; set; }
+        /// <summary>
+        /// Note Title
+        /// </summary>
+        [Required] // Not null setting
+        public string NoteTitle { get; set; }
+        /// <summary>
+        /// Note Contents
+        /// </summary>
+        [Required] // Not null setting
+        public string NoteContents { get; set; }
+        /// <summary>
+        /// User ID
+        /// </summary>
+        [Required] // Not null setting
+        public int UserNo { get; set; }
+
+        [ForeignKey("UserNo")]
+        public virtual User user { get; set; }
+    }
+}
