aa629cf0446fa9651d10795af3b1cb1dd1ded967
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2020-03-08 01:58:31 -0600

    20200308

---
+++ MVC_AW/MVC_AW/Models/UserLists.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_AW.Models
+{
+    public partial class UserLists
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Mobile { get; set; }
+        public bool? IsActive { get; set; }
+        public int? RoleId { get; set; }
+
+        public virtual UserRoles Role { get; set; }
+    }
+}
