edbd3b499079dd0fa1d37f0393065ca16f15a0f2
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2020-03-09 00:15:18 -0500

    MVC Entity Login

---
+++ 00.VS2017/MVC_Login/MVC_Login/ViewModels/LoginUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Login.ViewModels
+{
+    public partial class LoginUser
+    {
+        [DisplayName("User Name")]
+        [Required(ErrorMessage ="User Name is requried.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is requried.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
