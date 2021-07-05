575e2210e36b8507847e1f6791d7651fc7255b22
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2021-07-05 17:54:04 -0500

    asp .net mvc movie tutorial

---
+++ 00.VS2019/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: Movies
+        public ActionResult Index(string movieGenre, string movieRating, string searchString)
+        {
+            var GenreList = new List<string>();
+
+            var GenreQuery = from d in db.Movies
+                             orderby d.Genre
+                             select d.Genre;
+
+            GenreList.AddRange(GenreQuery.Distinct());
+
+            var RatingList = new List<string>();
+            var RatingQuery = from d in db.Movies
+                              orderby d.Rating
+                              select d.Rating;
+            RatingList.AddRange(RatingQuery.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreList);
+            ViewBag.movieRating = new SelectList(RatingList);
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            if (!string.IsNullOrEmpty(movieRating))
+            {
+                movies = movies.Where(r => r.Rating == movieRating);
+            }
+
+            //return View(db.Movies.ToList());
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 00.VS2019/MvcMovie/MvcMovie/Readme.txt
@@ -0,0 +1,11 @@
+﻿ASP .NET MVC 5
+새필드 추가
+https://docs.microsoft.com/ko-kr/aspnet/mvc/overview/getting-started/introduction/adding-a-new-field
+
+유효성 검사 추가
+https://docs.microsoft.com/ko-kr/aspnet/mvc/overview/getting-started/introduction/adding-validation
+
+세부 정보 및 삭제 메서드 검사
+https://docs.microsoft.com/ko-kr/aspnet/mvc/overview/getting-started/introduction/examining-the-details-and-delete-methods
+
+
