151ef759e4dde47a22d120a07c54d25f75a60f98
fourseason7 <38109624+fourseason7@users.noreply.github.com>
2021-07-13 22:52:25 -0500

    TX Eagle Supply 1. add DB Model 2. Create Menu Template

---
+++ 00.VS2019/TX_EAGLE_SUPPLY/TX_Eagle_Supply/TX_Eagle_Supply/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TX_Eagle_Supply
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 00.VS2019/TX_EAGLE_SUPPLY/TX_Eagle_Supply/TX_Eagle_Supply/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TX_Eagle_Supply.MvcApplication" Language="C#" %>
